c58ea436ea10a59d86f5b4e0e740fe3c04f7874e
justina-tran <jtran12@fordham.edu>
2022-10-19 02:01:16 -0400

    fix ngrams

--- pages/1_📊_EDA.py
+++ pages/1_📊_EDA.py
@@ -26,7 +26,8 @@ review_df = review_df.apply(
     lambda x: x.replace({
         'Manhattan, New York, NY':'New York, NY', 
         'Queens, Queens, NY':'Queens, NY',
-        'Brooklyn, Brooklyn, NY': 'Brooklyn, NY'})
+        'Brooklyn, Brooklyn, NY': 'Brooklyn, NY',
+        'Manhattan, NY': 'New York, NY'})
     )
 store_reviews = review_df['user_loc'].value_counts().reset_index().head(10)
 store_reviews.columns = ['location','# of reviews']
@@ -39,8 +40,8 @@ ratings.columns = ['rating','num of reviews']
 fig = px.pie(ratings, values='num of reviews', names='rating', title='Rating Distribution')
 st.plotly_chart(fig, use_container_width=True)
 
-
 # plot wordclouds
+st.subheader("Word Cloud")
 st.write("A word cloud of all words from the reviews after text preprocessing. Update the word cloud by entering any words to remove.")
 
 words = set(nltk.corpus.words.words()) # all english words
@@ -52,44 +53,65 @@ unique_words = set(ok_words)
 my_stopwords = stopwords.words('english')
 corpus_wordcloud = " ".join(str(review) for review in cleaned_review_df.cleaned_text)
 
+if 'user_stopwords' not in st.session_state:
+    st.session_state.user_stopwords = []
+
+def handle_submit(new_stopwords):
+    st.session_state.user_stopwords = new_stopwords
+
 with st.form(key="Selecting words to remove from the word cloud"):
     user_stopwords = st.multiselect(label="Enter stopwords:", options=unique_words, help="Enter words to remove from the word cloud.")
-    submit_button = st.form_submit_button(label="Submit")
-    
-if submit_button:
-    st.write('A word cloud without the words:', ", ".join(w for w in user_stopwords))
-    my_stopwords.update(user_stopwords)
-    wordcloud = WordCloud(width=1600, height=800, mode = "RGBA", background_color='white', stopwords=my_stopwords).generate(corpus_wordcloud)
-    fig, ax = plt.subplots(figsize = (8,8))
-    ax.imshow(wordcloud, interpolation='bilinear')
-    ax.axis("off")
-    st.pyplot(fig)
-else:
-    wordcloud = WordCloud(width=1600, height=800, mode = "RGBA", background_color='white', stopwords=my_stopwords).generate(corpus_wordcloud)
-    fig, ax = plt.subplots(figsize = (8,8))
-    ax.imshow(wordcloud, interpolation='bilinear')
-    ax.axis("off")
-    st.pyplot(fig)
+    submit_button = st.form_submit_button(label="Submit", on_click=handle_submit, args=[user_stopwords])
+    if submit_button:
+        st.write('A word cloud without the words:', ", ".join(w for w in st.session_state.user_stopwords))
+        for s in user_stopwords:
+            st.session_state.user_stopwords.append(s)
+        wordcloud = WordCloud(width=1600, height=800, mode = "RGBA", background_color='white', stopwords=st.session_state.user_stopwords).generate(corpus_wordcloud)
+        fig, ax = plt.subplots(figsize = (8,8))
+        ax.imshow(wordcloud, interpolation='bilinear')
+        ax.axis("off")
+        st.pyplot(fig)
+    else:
+        wordcloud = WordCloud(width=1600, height=800, mode = "RGBA", background_color='white', stopwords=st.session_state.user_stopwords).generate(corpus_wordcloud)
+        fig, ax = plt.subplots(figsize = (8,8))
+        ax.imshow(wordcloud, interpolation='bilinear')
+        ax.axis("off")
+        st.pyplot(fig)
 
-def run_ngram():
+# plot n-grams
+if 'user_ngram' not in st.session_state:
+    st.session_state.user_ngram = "Unigram"
+if 'user_rating' not in st.session_state:
+    st.session_state.user_rating = "⭐️"
+
+def run_rating():
     if st.session_state["selected_rating"]:
         st.session_state["user_rating"] = st.session_state.selected_rating
 
-# plot n-grams
-user_select_rating = st.selectbox("Select a star rating (1-5)", options=("⭐️", "⭐️⭐️", "⭐️⭐️⭐️", "⭐️⭐️⭐️⭐️", "⭐️⭐️⭐️⭐️⭐️"), on_change=run_ngram, key="selected_rating")
+def run_ngrams():
+    st.session_state["user_ngram"] = st.session_state.selected_ngram
+
+st.subheader("N-gram Analysis")
+col1, col2 = st.columns(2)
+with col1: 
+    user_select_rating = st.selectbox("Select a star rating (1-5):", options=("⭐️", "⭐️⭐️", "⭐️⭐️⭐️", "⭐️⭐️⭐️⭐️", "⭐️⭐️⭐️⭐️⭐️"), on_change=run_rating, key="selected_rating")
+with col2:
+    user_select_ngram = st.radio("Select a n-gram:", options=("Unigram", "Bigram", "Trigram"), on_change=run_ngrams, key="selected_ngram", horizontal=True)
+
 one_rating_corpus = [review for review in cleaned_review_df[cleaned_review_df['rating'] == '1 star rating']['cleaned_text']]
 two_rating_corpus = [review for review in cleaned_review_df[cleaned_review_df['rating'] == '2 star rating']['cleaned_text']]
 three_rating_corpus = [review for review in cleaned_review_df[cleaned_review_df['rating'] == '3 star rating']['cleaned_text']]
 four_rating_corpus = [review for review in cleaned_review_df[cleaned_review_df['rating'] == '4 star rating']['cleaned_text']]
 five_rating_corpus = [review for review in cleaned_review_df[cleaned_review_df['rating'] == '5 star rating']['cleaned_text']]
 
-if st.session_state["user_rating"] == "⭐️":
-    create_ngram(one_rating_corpus, 2)
-elif st.session_state["user_rating"] == "⭐️⭐️":
-    create_ngram(two_rating_corpus, 2)
-elif st.session_state["user_rating"] == "⭐️⭐️⭐️":
-    create_ngram(three_rating_corpus, 2)
-elif st.session_state["user_rating"] == "⭐️⭐️⭐️⭐️":
-    create_ngram(four_rating_corpus, 2)
-else:
-    create_ngram(five_rating_corpus, 2)
+star_dict = {"⭐️": one_rating_corpus,
+             "⭐️⭐️": two_rating_corpus,
+             "⭐️⭐️⭐️": three_rating_corpus,
+             "⭐️⭐️⭐️⭐️": four_rating_corpus,
+             "⭐️⭐️⭐️⭐️⭐️": five_rating_corpus}
+
+ngram_dict = {"Unigram": 1, 
+              "Bigram": 2,
+              "Trigram": 3}
+
+create_ngram(star_dict[st.session_state["user_rating"]], ngram_dict[st.session_state["user_ngram"]])
